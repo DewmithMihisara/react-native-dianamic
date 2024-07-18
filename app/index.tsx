9519cdae3f87ede131d7c721fad6b444e18189e9
Dewmith Mihisara <124574193+DewmithMihisara@users.noreply.github.com>
2024-07-19 02:48:12 +0530

    remove script

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,12 @@
-import { Text, View } from "react-native";
+import React from 'react';
+import { View, FlatList, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+const messages = [
+  { id: '1', name: 'Shane Martinez', message: 'On my way home but...', time: '5 min', avatar: 'https://example.com/avatar1.png' },
+  { id: '2', name: 'Katie Keller', message: 'I\'m watching Friends...', time: '15 min', avatar: 'https://example.com/avatar2.png' },
+  { id: '3', name: 'Stephen Mann', message: 'I\'m working now...', time: '1 hour', avatar: 'https://example.com/avatar3.png' },
+];
 
 export default function Index() {
   return (
